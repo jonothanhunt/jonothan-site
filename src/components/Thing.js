7dfee21ad7bdea0ff260ea5f6b644b386a50d168
Jonothan Hunt <hello@jonothankh.com>
2022-10-18 10:04:56 +0100

    update to all

--- src/components/Thing.js
+++ src/components/Thing.js
@@ -18,15 +18,9 @@ const Thing = (props) =>
 
             <p className="card-description">{props.description}</p>
 
-            <div className="card-stack-wrapper">
-                {props.stack.length > 0 && props.stack.map((stackItem, index) => (
-                    <div key={"stackItem_" + index} className="stack-item">{stackItem}</div>
-                ))}
-            </div>
-
             <div className="card-links-wrapper">
                 {Object.keys(props.links).length > 0 && Object.keys(props.links).map((link, index) => (
-                    <a key={"link_" + index} className="link" href={props.links[link]}>{link}</a>
+                    <a key={"link_" + index} className="card-link" href={props.links[link]}>{link}</a>
                 ))}
             </div>
 
