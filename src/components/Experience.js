795ebe3aa758fb8f37f6c413c16d65d07079ba5a
Jonothan Hunt <jonothankh@gmail.com>
2023-02-24 14:03:07 +0000

    Implement fully in R3F

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,44 +1,46 @@
-import { useEffect, useRef, useState, useMemo, useLayoutEffect } from "react";
+import './Experience.css'
+
+import { useEffect, useRef } from "react";
 import * as THREE from 'three'
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
-import { useControls, button } from 'leva'
+import { Perf } from 'r3f-perf'
 import
 {
+    useProgress,
     useGLTF,
     useTexture,
-    Environment,
-    MeshTransmissionMaterial,
     Float,
-    meshPhysicalMaterial
+    Sparkles
 } from '@react-three/drei'
-
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-
-import './Experience.css'
-// import { MeshPhysicalMaterial } from "three";
-import { Bloom, EffectComposer } from '@react-three/postprocessing'
 
 const Experience = (props) =>
 {
+
     return (
 
-        <div className="experience">
+        // canvas wrapper for css fade in
+        <div className={useProgress().loaded == 6 ? "experience active" : "experience"}>
 
-            <Canvas linear shadows orthographic camera={{ position: [10, 6, 20], zoom: 80 }} gl={{ preserveDrawingBuffer: true, antialias: true, toneMapping: THREE.NoToneMapping }}>
-                {/* <color attach="background" args={["red"]} /> */}
-                {/* orthographic camera={{ position: [10, 6, 20], zoom: 80 }} */}
-                {/* <PageCamera /> */}
+            {/* R3F canvas */}
+            <Canvas
+                eventSource={props.appRoot} // event source set to app div for mouse movement
+                eventPrefix="client" // to get global mouse movement
+                linear // to avoid R3F default colour changing to keep css colours the same
+                flat // sets toneMapping to THREE.NoToneMapping
+                orthographic // creates orthographic camera
+                camera={{ position: [-10, 4, 20], rotation: [-.3, -.45, -.16], zoom: 70 }} // sets camera values
+            >
+                {/* debug if /#debug in URL */}
+                {
+                    window.location.href.indexOf("#debug") > -1 && <Perf position={"top-left"} />
+                }
 
-                <InstancedBubbles />
-                {/* <EffectComposer> */}
-                <Text color="#fef4ef" scale={[14, 14, 14]} rotation={[0, 0, 0]} position={[0, 1, 0]} />
+                {/* sets scene background (mostly to avoid plane edge being visisble in physical material refraction) */}
+                <color attach="background" args={[0, 59 / 255, 73 / 255]} />
 
+                {/* All 3D objects in group with movement effects on scroll and mouse move */}
+                <Scene />
 
-
-                {/* <Bloom mipmapBlur intensity={0.1} /> */}
-
-                {/* </EffectComposer> */}
             </Canvas>
 
         </div>
@@ -46,77 +48,146 @@ const Experience = (props) =>
     )
 }
 
-export function Text({ color, ...props })
+// Imported glb of text with baked texture
+function Text({ color, ...props })
 {
     const texture = useTexture(require('../resources/dark_mode_jonothan.jpg'))
     texture.flipY = false
     const { nodes, materials } = useGLTF(require("../resources/jonothan.glb"));
 
-    const { size } = useThree()
-    const resizedFromX = size.width / 100
-
-    const textRef = useRef(null)
-    useFrame((state) =>
-    {
-        const windowScrollY = window.scrollY
-
-        textRef.current.rotation.x = windowScrollY / -1000
-        textRef.current.rotation.z = windowScrollY / 2600
-
-        textRef.current.scale.set(resizedFromX, resizedFromX, resizedFromX)
-        textRef.current.position.y = - (size.width / 400) + 4 //+ windowScrollY / 130
-
-    })
+    const textMaterial = new THREE.MeshBasicMaterial({ map: texture })
 
     return (
-        <group ref={textRef} {...props} dispose={null} >
-            <mesh geometry={nodes.Plane.geometry}>
-                <meshBasicMaterial map={texture} />
-            </mesh>
-            <mesh geometry={nodes.Text.geometry}>
-                <meshBasicMaterial map={texture} />
-            </mesh>
+        <group {...props} >
+            <mesh geometry={nodes.Plane.geometry} material={textMaterial} />
+            <mesh geometry={nodes.Text.geometry} material={textMaterial} />
         </group >
     );
 }
 
-function InstancedBubbles({ count = 7, temp = new THREE.Object3D() })
+// Instanced bubbles
+function Bubbles({ count = 6, bubble = new THREE.Object3D(), ...props })
 {
-    const ref = useRef()
+    const instancedBubblesRef = useRef()
+
+    const hdrEquirect = useTexture(require('../resources/gradient.jpg'))
+    hdrEquirect.mapping = THREE.EquirectangularReflectionMapping;
+
     useEffect(() =>
     {
         // Set positions
         for (let i = 0; i < count; i++)
         {
-            temp.position.set(
-                Math.random() * 3,
-                Math.random() * 3,
-                Math.random() * 3
+            bubble.position.set(
+                (i / count - 0.5) * 10 + ((Math.random() - 0.5) * 2),
+                Math.random() * 5,
+                (Math.random() - 0.5) * 10
             )
-            temp.scale.set(3, 3, 3)
+            const randomScale = 0.3 + (Math.random() * 1.2)
+            bubble.scale.set(randomScale, randomScale, randomScale)
 
-            temp.updateMatrix()
-            ref.current.setMatrixAt(i, temp.matrix)
+            bubble.updateMatrix()
+            instancedBubblesRef.current.setMatrixAt(i, bubble.matrix)
         }
         // Update the instance
-        ref.current.instanceMatrix.needsUpdate = true
+        instancedBubblesRef.current.instanceMatrix.needsUpdate = true
     }, [])
+
     return (
-        <instancedMesh ref={ref} args={[null, null, count]}>
-            <sphereGeometry />
-            <meshPhysicalMaterial
-                roughness={0}
-                transmission={1}
-                thickness={0.035}
-                depthTest={false}
-                depthWrite={false}
-                envMapIntensity={5}
-                ior={1}
-            />
-        </instancedMesh>
+        <group {...props}>
+            <instancedMesh ref={instancedBubblesRef} args={[null, null, count]}>
+                <sphereGeometry args={[1, 16, 16]} />
+                <meshPhysicalMaterial
+                    roughness={0}
+                    transmission={1}
+                    thickness={1}
+                    depthTest={false}
+                    depthWrite={false}
+                    envMapIntensity={5}
+                    ior={1.2}
+                    envMap={hdrEquirect}
+                />
+            </instancedMesh>
+        </group>
     )
 }
 
+// Preload textures
 useGLTF.preload(require("../resources/jonothan.glb"));
+useGLTF.preload(require('../resources/gradient.jpg'))
+
+// All 3D objects in group with movement effects on scroll and mouse move
+function Scene()
+{
+    const { viewport } = useThree()
+    const sceneRef = useRef(null)
+
+    const textRef = useRef(null)
+    const bubblesRef = useRef(null)
+
+    const { size } = useThree()
+    const resizedFromX = size.width * 0.0008
+
+    let mouseX = 0
+    let mouseY = 0
+
+    let smoothedMouseX = 0
+    let smoothedMouseY = 0
+
+    let smoothedWindowScrollY = 0
+
+
+    useFrame(({ mouse }) =>
+    {
+
+        const windowScrollY = window.scrollY
+        smoothedWindowScrollY = lerp(smoothedWindowScrollY, windowScrollY, 0.05)
+
+        mouseX = (mouse.x * viewport.width) / 2
+        mouseY = - (mouse.y * viewport.height) / 2
+        smoothedMouseX = lerp(smoothedMouseX, mouseX, 0.05)
+        smoothedMouseY = lerp(smoothedMouseY, mouseY, 0.05)
+
+        sceneRef.current.rotation.y = smoothedMouseX * 0.01
+        sceneRef.current.rotation.x = smoothedMouseY * 0.01
+        sceneRef.current.rotation.z = smoothedMouseY * 0.01
+        sceneRef.current.scale.set(resizedFromX, resizedFromX, resizedFromX)
+        sceneRef.current.position.y = (size.height / size.width) * 2 - 2.6
+
+        textRef.current.rotation.x = windowScrollY / -2400
+        textRef.current.rotation.z = windowScrollY / -4000
+
+        textRef.current.position.y = (windowScrollY / 60) * (1000 / size.width)
+
+        bubblesRef.current.rotation.x = smoothedWindowScrollY * 0.002
+
+    })
+
+    const lerp = function (start, end, amt)
+    {
+        return (1 - amt) * start + amt * end
+    }
+
+    return (
+        <group ref={sceneRef}>
+            <Float
+                floatIntensity={10}
+                floatingRange={5}
+
+            >
+                <group ref={bubblesRef}>
+                    <Bubbles />
+                </group>
+            </Float>
+
+            <group ref={textRef} scale={[14, 14, 14]} rotation={[0, 0, 0]} position={[0, 0, 0]}>
+                <Text color="#fef4ef" />
+                <Sparkles position={[0, .5, 0]} count={40} speed={1} size={10} scale={1} noise={0.006} />
+            </group>
+
+        </group>
+    )
+
+}
 
 export default Experience
--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -41,10 +41,10 @@ const HeaderBar = (props) =>
             <h1 className={titleVisible ? "name active" : "name"}>Jonothan</h1>
             <nav>
                 <ul className="header-links">
-                    <li className="header-link-list-item" ><a href="mailto:hello@jonothankh.com" className="header-link header-link-text">Email</a></li>
-                    <li className="header-link-list-item" ><a href="https://www.linkedin.com/in/jonothankh/" className="header-link header-link-text">LinkedIn</a></li>
-                    <li className="header-link header-link-icon"><a href="mailto:hello@jonothankh.com"><img className="header-icon" src={require('../icons/email.svg').default} alt="" /></a></li>
-                    <li className="header-link header-link-icon"><a href="https://www.linkedin.com/in/jonothankh/"><img className="header-icon" src={require('../icons/linkedin.svg').default} alt="" /></a></li>
+                    <li className="header-link-list-item" ><a href="mailto:hello@jonothankh.com" className="header-link header-link-text">hello@jonothan.dev</a></li>
+                    <li className="header-link-list-item" ><a href="https://github.com/jonothanhunt" className="header-link header-link-text">GitHub</a></li>
+                    <li className="header-link header-link-icon"><a href="mailto:hello@jonothan.dev"><img className="header-icon" src={require('../icons/email.svg').default} alt="" /></a></li>
+                    <li className="header-link header-link-icon"><a href="https://www.linkedin.com/in/jonothankh/"><img className="header-icon" src={require('../icons/github.svg').default} alt="" /></a></li>
                 </ul>
             </nav>
         </header>
