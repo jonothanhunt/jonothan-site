7222c1e13c87140796b785ae30a14c65fddde9c4
Jonothan Hunt <jonothankh@gmail.com>
2024-01-30 17:44:38 +0000

    Adjust pulse

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -51,10 +51,10 @@ const Header = (titleVisible) => {
                                         className={`inline-block w-3 h-3 rounded-full mr-2 animate-pulse transition-all ${
                                             location.pathname == "/"
                                                 ? "bg-secondary"
-                                                : "bg-transparent -mr-3"
+                                                : "bg-transparent -mr-3 w-0"
                                         }     `}
                                     />
-                                    Latest
+                                    <span>Latest</span>
                                 </Link>
                             </li>
                             <li
