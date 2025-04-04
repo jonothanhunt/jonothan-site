060bfc08f5cb6488e76525d301c799089eb54a59
Jonothan Hunt <hey@jonothan.dev>
2025-04-04 15:35:09 +0100

    Fancy new theme and clearing up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,15 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Atkinson_Hyperlegible } from "next/font/google";
-import Header from "./components/Header";
-import Experience from "./components/Experience";
-import { LightModeProvider } from "./context/LightModeContext"; // Import the provider
+import { ReactNode } from "react";
+import "./globals.css"; // Your global styles
 
-const atkinsonHyperlegible = Atkinson_Hyperlegible({
-    weight: ["400", "700"],
-    subsets: ["latin"],
-    display: "swap",
-});
+export const metadata = {
+    title: "Jonothan.dev",
+    description: "Creative developer and technologist",
+};
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html id="root" lang="en">
-            <body className={`${atkinsonHyperlegible.className} bg-amber-200 dark:bg-zinc-800`}>
-                <LightModeProvider>
-                    <Header />
-                    <div className="relative">
-                        <div className="absolute top-4 left-0 -z-10 w-full h-[min(70vw,60vh)]">
-                            <Experience />
-                        </div>
-                    </div>
-                    {children}
-                </LightModeProvider>
-            </body>
+        <html lang="en">
+            <body>{children}</body>
         </html>
     );
 }
