2438d2c37b4f09073c906083226967484c18384c
Jonothan Hunt <hey@jonothan.dev>
2025-04-04 16:03:06 +0100

    fix favicon setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,15 @@ import { ReactNode } from "react";
 import "./globals.css"; // Your global styles
 
 export const metadata = {
-    title: "Jonothan.dev",
-    description: "Creative developer and technologist",
+  title: "Jonothan.dev",
+  description: "Creative developer and technologist",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <meta name="apple-mobile-web-app-title" content="Jonothan.dev" />
+      <body>{children}</body>
+    </html>
+  );
 }
