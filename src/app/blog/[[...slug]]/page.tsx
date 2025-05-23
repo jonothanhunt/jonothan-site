6ac96f53bd1f520f8f55fc9c760d34a062194943
Jonothan Hunt <hey@jonothan.dev>
2025-05-23 17:20:34 +0100

    fixed the hydration issue with links in links

--- src/app/blog/[[...slug]]/page.tsx
+++ src/app/blog/[[...slug]]/page.tsx
@@ -96,31 +96,27 @@ export default async function Thing({ params }: PageProps) {
             return (
               <div
                 key={post.slug}
+                className="border-purple-200 rounded-4xl overflow-clip shadow-xl shadow-purple-950/10 transition-all duration-1000"
               >
-                <div>
-                  <Link
-                    href={isSelected ? "/blog" : `/blog/${post.slug}`}
-                    className="block"
+                <Link
+                  href={isSelected ? "/blog" : `/blog/${post.slug}`}
+                >
+                  <div
+                    className={`flex flex-col-reverse p-5 sm:flex-row gap-2 justify-between bg-gradient-to-br sm:bg-gradient-to-bl from-purple-300/20 via-purple-100 to-purple-100 transition-all duration-1000 ${
+                      isSelected
+                        ? "rounded-t-4xl rounded-b-none"
+                        : "rounded-t-4xl rounded-b-4xl"
+                    }`}
                   >
-                    <div className="border-purple-200 rounded-4xl overflow-clip shadow-xl shadow-purple-950/10 transition-all duration-1000">
-                      <div
-                        className={`flex flex-col-reverse p-5 sm:flex-row gap-2 justify-between bg-gradient-to-br sm:bg-gradient-to-bl from-purple-300/20 via-purple-100 to-purple-100 transition-all duration-1000 ${
-                          isSelected
-                            ? "rounded-t-4xl rounded-b-none"
-                            : "rounded-t-4xl rounded-b-4xl"
-                        }`}
-                      >
-                        <h2 className="font-[family-name:var(--font-lastik)] text-3xl text-purple-950 text-pretty">
-                          {post.title}
-                        </h2>
-                        <p className="text-purple-950 min-w-fit w-fit h-fit px-3 py-2 rounded-full bg-pink-200/70 shadow-xl shadow-purple-950/20 text-sm">
-                          {formatCustomDate(post.date)}
-                        </p>
-                      </div>
-                      {isSelected && <DynamicMDXContent slug={post.slug} />}
-                    </div>
-                  </Link>
-                </div>
+                    <h2 className="font-[family-name:var(--font-lastik)] text-3xl text-purple-950 text-pretty">
+                      {post.title}
+                    </h2>
+                    <p className="text-purple-950 min-w-fit w-fit h-fit px-3 py-2 rounded-full bg-pink-200/70 shadow-xl shadow-purple-950/20 text-sm">
+                      {formatCustomDate(post.date)}
+                    </p>
+                  </div>
+                </Link>
+                {isSelected && <DynamicMDXContent slug={post.slug} />}
               </div>
             );
           })}
