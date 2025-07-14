ea6f82012ce6365617c41d17f1907d71a2c1959f
Jonothan Hunt <hey@jonothan.dev>
2025-07-14 14:55:13 +0100

    Implement link to Things

--- src/types/thing.ts
+++ src/types/thing.ts
@@ -1,9 +1,15 @@
 export type ThingType = 'Post' | 'Video' | '3D Model' | 'Image' | 'AR Filter';
 
+export interface ThingLink {
+  title: string;
+  url: string;
+}
+
 export interface ThingMetadata {
   title: string;
   date: string;
   excerpt: string;
   type: ThingType[];
   image?: string;
+  links?: ThingLink[];
 }
